4cb74013a38c63ece2746828c45bc94295d7bb6e
Alejandro Colomar <colomar.6.4.3@gmail.com>
2020-05-17 17:10:36 +0200

    Remove sudo; user should use sudo

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,22 +5,22 @@ external=eth0
 internal=eth1
 service=dns
 ####################
-sudo yum update -y
-sudo yum install -y firewalld
-sudo systemctl enable firewalld
-sudo systemctl restart firewalld
-sudo firewall-cmd --change-interface=$external --zone=external --permanent
-sudo firewall-cmd --change-interface=$internal --zone=internal --permanent
-sudo firewall-cmd --reload
+yum update -y
+yum install -y firewalld
+systemctl enable firewalld
+systemctl restart firewalld
+firewall-cmd --change-interface=$external --zone=external --permanent
+firewall-cmd --change-interface=$internal --zone=internal --permanent
+firewall-cmd --reload
 ####################
-sudo yum install -y bind bind-utils
-sudo systemctl enable named
-sudo firewall-cmd --add-service=$service --zone=internal --permanent
+yum install -y bind bind-utils
+systemctl enable named
+firewall-cmd --add-service=$service --zone=internal --permanent
 ####################
-sudo cp -rv * /
-sudo sed -i "/^DNS1=/s/^DNS1=.*$/DNS1=$localhost/" /etc/sysconfig/network-scripts/ifcfg-$internal
-sudo sed -i "/^DOMAIN=/s/^DOMAIN=.*$/DOMAIN=$domain/" /etc/sysconfig/network-scripts/ifcfg-$internal
+cp -rv * /
+sed -i "/^DNS1=/s/^DNS1=.*$/DNS1=$localhost/" /etc/sysconfig/network-scripts/ifcfg-$internal
+sed -i "/^DOMAIN=/s/^DOMAIN=.*$/DOMAIN=$domain/" /etc/sysconfig/network-scripts/ifcfg-$internal
 ####################
-sudo init 6
+init 6
 ####################
 
